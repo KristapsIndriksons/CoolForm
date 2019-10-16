d8fc00c443e7a36111900629bb247ae55ed0c040
Kristers Lielups <kristers@scandiweb.com>
2019-10-16 13:40:31 +0300

    KL-Model Create initial user model

---
+++ Models/User.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace CoolForm;
+
+/**
+ * Class User
+ * Holds current users data
+ *
+ * @package CoolForm
+ */
+class User
+{
+    /**
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    protected $username;
+
+    /**
+     * @var string
+     */
+    protected $passwordHash;
+}
