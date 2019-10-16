a1639c67538ae2039611725c433534047a09072a
Kristers Lielups <kristers@scandiweb.com>
2019-10-16 13:45:50 +0300

    KL-Model DB stuff

---
+++ DB/Connector.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace CoolForm\DB;
+
+class Connector
+{
+    const HOST = 'localhost';
+    const USER = 'root';
+    const PASSWORD = '';
+    const DB = 'CoolForm';
+
+    public function __construct()
+    {
+    }
+
+
+}
---
+++ DB/Repositories/UserRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace CoolForm\DB\Repositories;
+
+/**
+ * Class UserRepository
+ * Fetch user data from DB
+ *
+ * @package CoolForm
+ */
+class UserRepository
+{
+
+}
