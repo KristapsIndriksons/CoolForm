97e2995e64f4b513e26cd7f976d356621c8edd1e
Kristaps Indriksons <kristaps.indriksons@testdevlab.com>
2019-11-05 22:51:01 +0200

    Deleted unnecessary lines of code

--- DB.php
+++ DB.php
@@ -40,105 +40,13 @@ class DB {
     }
     return $this;
   }
-  //Izveido SELECT vaicājumu un padod Query metodei SQL un parametrus
-  public function action($action, $table, $where = array(), $logic = array()){
-    if(count($where) == 3){
-      $operators = array('=','>','<','>=','<=');
-      $field = $where[0];
-      $operator = $where[1];
-      $value = $where[2];
-      if(in_array($operator, $operators)){
-        $sql = "{$action} FROM {$table} WHERE {$field} {$operator} ?";
-        if(!$this->query($sql, array($value))->error()){
-          return $this;
-        }
-      }
-    }else if(count($where) == 6 && count($logic) == 1){
-      $operators = array('=','>','<','>=','<=');
-      $logics = array('and','or');
-      $field1 = $where[0];
-      $operator1 = $where[1];
-      $value1 = $where[2];
-      $field2 = $where[3];
-      $operator2 = $where[4];
-      $value2 = $where[5];
-      $logic_op = $logic[0];
-      if(in_array($operator1, $operators) && in_array($operator2, $operators) && in_array($logic_op, $logics)){
-        $sql = "{$action} FROM {$table} WHERE {$field1} {$operator1} ? {$logic_op} {$field2} {$operator2} ?";
-        if(!$this->query($sql, array($value1,$value2))->error()){
-          return $this;
-        }
-      }
-    } else {
-      $sql = "{$action} FROM {$table}";
-      if(!$this->query($sql)->error()){
-        return $this;
-      }
-    }
-    return false;
-  }
-  //Insert vaicājuma izveide
-  public function insert($table, $fields = array()){
-    if(count($fields)){
-      $keys = array_keys($fields);
-      $values = null;
-      $x = 1; //counter
-
-      foreach ($fields as $field) {
-        $values .= "?";
-        if($x < count($fields)){
-          $values .= ", ";
-        }
-        $x++;
-      }
-      // implode - pārvēš masīva elementus par string
-      $sql = "INSERT INTO {$table}(`". implode('`, `',$keys) ."`) VALUES({$values})";
-      // Izpilda vaicājumu un pārbauda vai tas bija veiksmīgs
-      if(!$this->query($sql, $fields)->error()){
-        return true;
-      }
-    }
-    return false;
-  }
-  // Update vaicājuma izveidi
-  public function update($table, $id, $fields = array()){
-    $set = "";
-    $x = 1;
-
-    foreach($fields as $name => $value){
-      $set .= "{$name} = ?";
-      if($x < count($fields)){
-        $set .= ", ";
-      }
-      $x++;
-    }
-    
-    $sql = "UPDATE {$table} SET {$set} WHERE user_id = {$id}";
-    
-    if(!$this->query($sql, $fields)->error()){
-      return true;
-    }
-    return false;
-  }
   //Atgriež objekta _results vērtību, results ir objektu masīvs
   public function results(){
     return $this->_results;
   }
-  // izsauc action funkciju
-  public function get($table, $where, $logic){
-    return $this->action('SELECT *', $table ,$where,$logic);
-  }
-
-  public function delete($table, $where){
-    return $this->action('DELETE', $table ,$where);
-  }
 
   public function count(){
     return $this->_count;
-  }
-
-  public function first(){
-    return $this->results()[0];
   }
 
   public function error(){
--- Validate.php
+++ Validate.php
@@ -10,47 +10,6 @@ class Validate{
     $this->_db = DB::getInstance();
   }
 
-  public function check($source, $items = array()){
-    foreach ($items as $item => $rules) {
-      foreach ($rules as $rule => $rule_value){
-        $value = trim($source[$item]);
-        if($rule === 'required' && empty($value)){
-          $this->addError("{$item} is required");
-        } else if(!empty($value)) {
-          switch($rule){
-            case 'min':
-              if(strlen($value) < $rule_value){
-                $this->addError("{$item} must be a min of {$rule_value} chars");
-              }
-            break;
-            case 'max':
-            if(strlen($value) > $rule_value){
-              $this->addError("{$item} must be a max of {$rule_value} chars");
-            }
-            break;
-            case 'unique':
-              $check = $this->_db->get($rule_value,array($item,'=',$value));
-              if($check->count()){
-                $this->addError("{$item} already exists");
-              }
-            break;
-            case 'length':
-            if(strlen($value) !== $rule_value){
-              $this->addError("{$item} must be a lenght of {$rule_value} numbers");
-            }
-            break;
-            default:
-            break;
-          }
-        }
-      }
-    }
-    if(empty($this->_errors)){
-      $this->_passed = true;
-    }
-    return $this;
-  }
-
   public function check_user($source, $items = array()){
       $username = trim($source['username']);
       $password = trim($source['password']);
